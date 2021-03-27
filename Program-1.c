f35d2ed27a6f970c30b8a8b2f7ccfc729cfd906a
Chandana141 <chandanadhanapal25@gmail.com>
2021-03-27 03:35:19 +0000

    done

--- Program-1.c
+++ Program-1.c
@@ -1,17 +1,17 @@
-//INPUT: 
-//OUTPUT: You entered: 
+//INPUT:5 
+//OUTPUT: You entered:5
 #include <stdio.h>
 int main  ()      // correct the code
 {   
     int number;
    
-    printf("Enter the integer: ");     // Correct the code
+    printf("Enter the integer:\n");     // Correct the code
     
     // reads and stores input
     scanf("%d", &number);
 
     // displays output
-    printf("You entered: %d", number);// Correct the code
+    printf("You entered: %d\n", number);// Correct the code
     
     return 0;
     }
